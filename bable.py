91e05396a8fccfd07a0be5f7d4b73efe0c336ec2
ballboy <ballboyst365@gmail.com>
2025-04-02 23:35:19 +0900

    update

--- bable.py
+++ bable.py
@@ -1,4 +1,4 @@
-def hada_sort(arr):
+def h_sort(arr):
     n = len(arr)
     for i in range(n):
         for j in range(n - i - 1):
@@ -6,5 +6,5 @@ def hada_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]  # タプル代入で交換
     return arr
 
-hada=[1,5,2,4,7,3]
-print(hada_sort(hada))
+nums=[1,5,2,4,7,3]
+print(h_sort(nums))
