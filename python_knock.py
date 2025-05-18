94aa28fb5bc42f8d6d7858a8f339f182f0f8b571
ballboy <ballboyst365@gmail.com>
2025-05-18 21:41:43 +0900

    do knock

--- python_knock.py
+++ python_knock.py
@@ -1,4 +1,7 @@
-st = {1,2,3}
-print(9 in st)
-print(1 in st)
-# 問題40まで終了
+dict = {'id': '0001', 'name': 'guest'}
+for key, value in dict.items():
+    print(f'key:{key},value:{value}')
+# 問題43まで終了
+# タプルは組を意味する。複数の要素が決まった順番に並ぶ。表現は(x,y)
+# リストは配列を意味する。表現は[x,y]　例えばlist=[1,2,3]ならlist[1]で２番目の要素を取得できる
+# 辞書はキーと値から構成される。キーがあるため要素を一意に特定可能。表現は{key:value}
