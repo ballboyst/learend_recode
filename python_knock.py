269baac386d784f6ae9a01ee2ca1b66e8e210571
ballboy <ballboyst365@gmail.com>
2025-05-22 23:20:35 +0900

    do knock

--- python_knock.py
+++ python_knock.py
@@ -5,15 +5,11 @@ for key, value in dict.items():
 lst = [n for n in range(1,10) if n%2 == 0]
 print(lst)
 
-x = "1,2,3"
-lst = x.split(",")
-print(lst)
-lst2 = '&'.join(x)
-print(lst2)
+x = -1
+if x < 0:
+    raise ValueError('府の数が入力されました')
 
-lst = [1,2,3]
-print(max(lst))
-# 問題46まで終了
+# 問題50まで終了
 # タプルは組を意味する。複数の要素が決まった順番に並ぶ。表現は(x,y)
 # リストは配列を意味する。表現は[x,y]　例えばlist=[1,2,3]ならlist[1]で２番目の要素を取得できる
 # 辞書はキーと値から構成される。キーがあるため要素を一意に特定可能。表現は{key:value}
