ea6f89be580dd0da7a7b86eb9afd180435f6132e
ballboy <ballboyst365@gmail.com>
2025-05-30 20:52:25 +0900

    do knock

--- python_knock.py
+++ python_knock.py
@@ -1,21 +1,24 @@
-# target = "hello"
-# for num in target:
-#     print(num)
-# lst = "hello"
-# dict = {}
-# for char in lst:
-#     if char in dict:
-#         dict[char] += 1
-#     else:
-#         dict[char] = 1
-# print(dict)
-target_list = [1, 2, 3, None, 5]
-for num in target_list:
-    try:
-        print(num * 2)
-    except TypeError as e:
-        print(e)
-# 問題59まで終了
+employees = [
+    {'id': '0001', 'name': '田中', 'location_id': 'L01'},
+    {'id': '0002', 'name': '山田', 'location_id': 'L02'},
+    {'id': '0003', 'name': '小林', 'location_id': 'L01'},
+    {'id': '0004', 'name': '藤本', 'location_id': 'L03'},
+    {'id': '0005', 'name': '佐々木', 'location_id': 'L02'},
+    {'id': '0006', 'name': '松田', 'location_id': 'L04'},
+    {'id': '0007', 'name': '中村', 'location_id': 'L01'},
+    {'id': '0008', 'name': '石川', 'location_id': 'L03'},
+    {'id': '0009', 'name': '清水', 'location_id': 'L05'},
+    {'id': '0010', 'name': '近藤', 'location_id': 'L02'}
+]
+for check in employees:
+    if check['location_id'] in {'L01','L02'}:
+        print(check["id"],check["name"])
+
+
+
+
+# 問題60まで終了
+# セットは集合を意味する。表現は{x,y}
 # タプルは組を意味する。複数の要素が決まった順番に並ぶ。表現は(x,y)
 # リストは配列を意味する。表現は[x,y]　例えばlist=[1,2,3]ならlist[1]で２番目の要素を取得できる
 # 辞書はキーと値から構成される。キーがあるため要素を一意に特定可能。表現は{key:value}
