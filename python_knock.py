4b58480860b1e2ee23e54fcec2eef5500c7cce7e
ballboy <ballboyst365@gmail.com>
2025-06-18 22:45:11 +0900

    do knock

--- python_knock.py
+++ python_knock.py
@@ -1,34 +1,8 @@
+from library import import_test
 
-class SimpleClass():
-    count = 0  # class変数
-    def __init__(self,name):
-        self.name = name
-        SimpleClass.count += 1  # class.valueでclass変数にアクセスする
-    def print_count(self):
-        print(f'{SimpleClass.count}回目のクラスが作成されました')
-    def print_name(self):
-        print(f'名前が{self.name}のオブジェクトを作成しました')
+import_test.sample()
 
-Tom = SimpleClass('tom')
-Tom.print_count()
-Alice = SimpleClass('Alice')
-Alice.print_count()
-# 上記でも同じ出力は得られるが、以下のコードが模範解答
-
-class SimpleClass():
-    count = 0  # class変数
-    def __init__(self,name):
-        self.name = name
-        SimpleClass.count += 1  # class.valueでclass変数にアクセスする
-    @classmethod
-    def print_count(cls):
-        print(cls.count)
-
-SimpleClass.print_count()
-Sample = SimpleClass("aa")
-SimpleClass.print_count()
-
-# 問題67まで終了
+# 問題70まで終了
 # セットは集合を意味する。表現は{x,y}
 # タプルは組を意味する。複数の要素が決まった順番に並ぶ。表現は(x,y)
 # リストは配列を意味する。表現は[x,y]　例えばlist=[1,2,3]ならlist[1]で２番目の要素を取得できる
