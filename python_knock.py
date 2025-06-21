0a7c09099992a233c6e7abccb06b12a783db9a81
ballboy <ballboyst365@gmail.com>
2025-06-21 23:30:07 +0900

    do knock

--- python_knock.py
+++ python_knock.py
@@ -1,7 +1,24 @@
-with open('test.txt','r') as file:
-    content = file.read()
-    print('test.txt')
-# 問題70まで終了
+import csv
+lst = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+with open('test.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for row in lst:
+        writer.writerow(row)
+
+
+with open('test.csv', 'r', newline='') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+
+
+
+# 問題84まで終了
 # セットは集合を意味する。表現は{x,y}
 # タプルは組を意味する。複数の要素が決まった順番に並ぶ。表現は(x,y)
 # リストは配列を意味する。表現は[x,y]　例えばlist=[1,2,3]ならlist[1]で２番目の要素を取得できる
